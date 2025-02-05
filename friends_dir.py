fd6df84221fd780e7bee3f4837c6db3367f9a8e4
aearns <aearns@gmail.com>
2025-02-05 14:28:24 +0000

    Edits

--- friends_dir.py
+++ friends_dir.py
@@ -1,22 +1,32 @@
 
-user = str (input("Please enter your name: "))
-print(f"Hello, {user}!, welcome")
+#Friend list of a user
 
-number_of_friends = int (input("How many friends do you have? "))
+user = str(input("Please enter your name: "))                       #user input
+print(f"Hello, {(user.title())}!\nWelcome")
+
+while True:                                                         #
+    number_of_friends = input("How many friends do you have? ")
+    if number_of_friends.isdigit():
+        number_of_friends = int(number_of_friends)
+        break                                                           # Exit the loop if the input is a valid number
+    else:
+        print("Please enter a valid number")
+
+print(f"You have {number_of_friends} friends.\nYou will be prompted to enter the name of each of your {number_of_friends} friends")
+    
 friend_list = []
-print(f"You will be prompted to enter the name of each of your {number_of_friends} friends")
 confirm_number = input("Type 'Yes' to continue, 'No' to exit or 'Change' the number of friends:\n")
 if confirm_number.upper() == "YES":
     for friend in range(number_of_friends):
         name_of_friend = str(input("Please enter your friend's name: "))
-        friend_list.append(name_of_friend)
+        friend_list.append((name_of_friend.title()))
 elif confirm_number.upper() == "CHANGE":
     print("Let's change the number of friends you have")
     number_of_friends = int(input("How many friends do you have?\n "))
     print(f'You now have {number_of_friends} friends')
 else:
     if confirm_number.upper() == "NO":
-        print("Goodbye, {user}! Have a nice day!")
+        print(f"Goodbye, {user}! Have a nice day!")
     exit()
 
 
@@ -25,6 +35,6 @@ friend_name = str(input(f"Please enter the names of your {number_of_friends} sep
 for names in friend_list:
     friend_list.append(name_of_friend)
     print(names)
-print("Nice to have friends such as:")
+print(f"Nice to have friends such as {name_of_friend} ")
     
     
