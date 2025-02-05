ddfdde85ab103d608765146aab2dc757efa37f69
aearns <aearns@gmail.com>
2025-02-05 01:36:05 +0000

    Adding Assignment_4 file

---
+++ assignment_4.py
@@ -0,0 +1,6 @@
+
+user = input("Please enter your name: ")
+print(f"Hello, {user}!")
+friend_list = input("Please enter your friend list(separated by a comma(,)):")
+friend_list = friend_list.split(",")
+print(f"Nice! Your friends are, {friend_list}")
