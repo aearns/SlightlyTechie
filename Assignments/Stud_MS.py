a9cb2eaa01d8032397960e5f692edfb6c7647429
aearns <aearns@gmail.com>
2025-02-11 20:00:50 +0000

    changed var

--- Assignments/Stud_MS.py
+++ Assignments/Stud_MS.py
@@ -1,11 +1,11 @@
-# Personalized library
+# Personalized system
 '''
     This program allows users to create a School Management System. Users can add, view, edit and delete student records.
     Users can also search for students in their system.
     Program stores user details (name and password) and student details in a list. - not efficient but for learning purposes
     Program does not use any functions. 
 '''
-print("Welcome to your personalized library.")
+print("Welcome to your personalized system.")
 #functions
 # start_program()
 # user_details()
@@ -37,35 +37,35 @@ if username not in users:
     if password == confirm_password:
         users.update({"name": username, "password": password})
         print(f"Welcome {username}! Verified successfully.")                        
-    print("Let's get started with your library!")
+    print("Let's get started with your system!")
 else:
     print(f"Welcome back {username}!")
  
 
 while True:
     # Program menu
-    print("Choose menu list\n1. Add new student\n2. View all students\n3. Edit student record\n4. Delete record\n5. Restart program\n6. Exit library")
+    print("Choose menu list\n1. Add new student\n2. View all students\n3. Edit student record\n4. Delete record\n5. Restart program\n6. Exit system")
 # Program menu - User inputs
     choice = (input("Enter your choice: "))
     if choice == "1":
         studentName = input("Enter name of student:\n").title()
-        print(f"Let's add {studentName} to your library...\nIndexing...")
+        print(f"Adding {studentName} to your system...\nIndexing...")
 
         if studentName in student_dir:
-            print(f"You already have {studentName} in your library")
+            print(f"You already have {studentName} in your system")
         else:
             while True:
-                confirm_book = int (input(f"Select\n1. To confirm and add {studentName} to library\n2. To go back\nYour choice here: "))
+                confirm_book = int (input(f"Select\n1. To confirm and add {studentName} to system\n2. To go back\nYour choice here: "))
                 if confirm_book == 1:
-                    print(f"You have added {studentName} to your library")
+                    print(f"You have added {studentName} to your system")
                     try:
-                        author = str (input(f"Enter author of {studentName}:\n")).title()
+                        author = str (input(f"Enter programme of {studentName}:\n")).title()
                     except ValueError:
-                        print("Author must be a name")  
+                        print("Programme must be a text")  
                         continue      
                     while True:
                         try:
-                            year = int (input("Enter year of publication:\n"))
+                            year = int (input("Enter level:\n"))
                             if year >= 1000:
                                break
                             else:
@@ -74,7 +74,7 @@ while True:
                             print("Year of publication must be a number")
                         
                     student_dir[studentName]= (author, year)
-                    print(f"Adding {studentName} by {author} published in {year} to your library...\nIndexing...")
+                    print(f"Adding {studentName} by {author} published in {year} to your system...\nIndexing...")
                     # for book, publisher, date in  :
                     break
                 
@@ -85,18 +85,18 @@ while True:
                     print("Please enter a valid input")
                 break
         
-    #User input 2 - View all books in library
+    #User input 2 - View all books in system
     elif choice == "2":
-        print("Loading all books in library")
+        print("Loading all books in system")
         if len(student_dir) >= 1:
-            print(f"You have {len(student_dir)} in your library")
+            print(f"You have {len(student_dir)} in your system")
             for text, (title, (writer, year)) in enumerate(student_dir.items(), start=1):
                 print(f"{text}. {title}, {writer}, {year}")
         else:
             print("You have not added a book yet.\nLet's add a book")
         # code should run program menu screen line 41
 
-    #Option 3 to edit a book in library
+    #Option 3 to edit a book in system
     elif choice == "3":
         print("Let's pick a book to edit")
         if len(student_dir) >= 1:
@@ -114,7 +114,7 @@ while True:
 
     elif choice == "4":
         search_query = input("Enter title, author or publication year of book to delete:\n").split()
-        print("Listing all matches of books, authors and years found in your library")
+        print("Listing all matches of books, authors and years found in your system")
         for word in search_query:
             if word in student_dir.item():
                 print(f"{len(word)} matches found")
@@ -124,7 +124,7 @@ while True:
                     delete_options = input(f"{num}. - {student_dir} \n")
                     if delete_options == num:
                         student_dir.pop()
-                        print(f"{student_dir} deleted from library")
+                        print(f"{student_dir} deleted from system")
                     else:
                         pass
     else:
