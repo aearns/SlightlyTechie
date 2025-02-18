1589c67ae3c5e4176885343c4f76a178a2911418
aearns <aearns@gmail.com>
2025-02-18 07:41:29 -1200

    logging update

--- Assignments/Stud_MS.py
+++ Assignments/Stud_MS.py
@@ -3,10 +3,14 @@ import logging
 
 '''
     This program allows users to create a School Management System. Users can add, view, edit and delete student records.
-    Users can also search for students in their system.
-    Program stores user details (name and password) and student details in a list. - not efficient but for learning purposes
-    Program does not use any functions. 
+    The program creates a secured (basic) admin/user. Access is granted to verified user to CRUD student records in the system.
+    Program stores user details (name and password) and student details in a dict.
+    This program implements data types, controls, functions, error handling and  logging module. 
 '''
+
+logging.basicConfig(filename='stud_ms.log', level=logging.DEBUG, format='%(asctime)s -%(levelname)s - %(name)s - %(message)s')
+logging.info("App launch")
+
 print("Welcome to your personalized system.")
 #functions
 users = {}
@@ -19,11 +23,11 @@ def user():
     '''
     username = str (input("Enter your name?\n")).title()
     if username not in users:
+        logging.info("User not in DB. Creating a new user")
 # Creating new user
         print(f"Hello {username}! Let's set you up.")           # add new user to the list of users
-    # users.append(username)
         while True:
-            password = input("Enter a new password:\n")
+            password = str(input("Enter a new password:\n"))
             if len(password) >= 8 and password.isalnum():
                 print("Let's confirm your password!")
                 confirm_password = input("Confirm your password:\n")
@@ -38,10 +42,13 @@ def user():
         
         if password == confirm_password:
             users.update({"name": username, "password": password})
+            logging.info("New user successfully created")                        
             print(f"Welcome {username}! Verified successfully.")                        
             print("Let's get started with your system!")
+            
     else:
         print(f"Welcome back {username}!")
+        logging.info("User found in system")
         confirm_password = input("Enter your password:\n")
         while password != confirm_password:
                 print("Passwords do not match. Please try again.")
@@ -50,17 +57,20 @@ def user():
                 break
         if password == confirm_password:
             print(f"Welcome back {username}! Verified successfully.")                        
+            logging.INFO("User logged in successfully")
             
+
 def add_student():
     '''
     This function adds a new student to the system
     '''
-    global student_dir
+    global student_dir, username
     studentName = input("Enter name of student:\n").title()
     print(f"Adding {studentName} to your system...\nIndexing...")
-
+    logging.info("Creating new student details")
     if studentName in student_dir:
         print(f"You already have {studentName} in your system")
+        logging.info("Student already in system")
     else:
         while True:
             confirm_student = int (input(f"Select\n1. To confirm and add {studentName} to system\n2. To go back\nYour choice here: "))
@@ -69,7 +79,8 @@ def add_student():
                 try:
                     programme = str (input(f"Enter programme of {studentName}:\n")).title()
                 except ValueError:
-                    print("Programme must be a text")  
+                    print("Programme must be a text")
+                    logging.error("User error. Wrong details entered")  
                     continue                      
                 try:
                     level = int (input("Enter level:\n"))
@@ -79,9 +90,11 @@ def add_student():
                         print("Enter valid level")
                 except ValueError:
                     print("Student level must be 100 - 400")
+                    logging.error(f"User input {level} is incorrect.")
                     break
                 student_dir[studentName]= (programme, level)
                 print(f"Adding {studentName}, {programme} in level {level} to your system...\nIndexing...")
+                logging.info(f"New student successfully added by {username}")
                 # for book, publisher, date in  :
                 break
             
@@ -90,6 +103,7 @@ def add_student():
         
             else:
                 print("Please enter a valid input")
+                logging.error("User failed to enter valid input")
             break
 
 def view_all():
@@ -97,11 +111,13 @@ def view_all():
     This function view all records of students
     '''
     global student_dir
+    logging.info("User request for all records")
     if len(student_dir) >= 1:
         print("Loading all students in system")
         print(f"You have {len(student_dir)} in your system")
         for num, (student_dir, (programme, level)) in enumerate(student_dir.items(), start=1):
             print(f"{num}. {programme}, {level}")
+        logging.info("Data request/pull successful")
     else:
        print("You have no record of students.\nLet's add students.")
     add_student()
@@ -141,17 +157,17 @@ def delete_record(student_dir, word):
         else:
             print("No record found")
 
-def search_record():
+def search_record(student_dir, word):
     '''
     Use this function to search details of record in the system
     '''
-    global student_dir
+    #global student_dir
     search_query = input("Enter student name, programme or level of studente:\n").split()
     print("Listing all matches of students, programmes and levels found in your system")
     for word in search_query:
         if word in student_dir.item():
             print(f"{len(word)} matches found")
-            for word, (title, (programme, level)) in enumerate(student_dir.items(), start=1):
+            for word, (student_dir, (programme, level)) in enumerate(student_dir.items(), start=1):
                 print(f"-{word}")
 
 def exit():
