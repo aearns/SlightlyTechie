1109d8fba8c8bd6cf96b6b3e2b8c059feb170a89
aearns <aearns@gmail.com>
2025-03-07 22:18:28 +0000

    bank ATM file

---
+++ Assignments/bank_atm.py
@@ -0,0 +1,41 @@
+# Bank ATM program
+
+# print('Welcome to StillBank\nPlease choose an option\n1.Withdraw Cash\n2.Check Account Balance')
+
+# user_accounts = {}
+# pin = []
+
+# account: int = input("Enter your account code:\n")
+# user_accounts[account] = pin
+# user_pin: int = input("Enter your new secret PIN:\n")
+# confirm_pin: int = input("Enter PIN to confirm:\n")
+
+# def user():
+#     for number in user_accounts:
+#         if number == account:
+#             print("Account found")
+
+
+# def create_pin(account, user_pin, confirm_pin, user_accounts):
+#     if user_pin == confirm_pin:
+#         pin.update(user_accounts[account])
+#         print("PIN successfully created")
+        
+#     else:
+#         print("PIN does not match. Try again")
+
+
+    
+class ATM:
+    def __init__(self):
+        # Dictionary to store user account information
+        self.user_accounts = {}
+        self.logged_in_user = None
+
+    def create_account(self, account, user_pin):
+        if account in self.user_accounts:
+            print(f"Account {account} already exists.")
+        else:
+            # Initialize user account with a PIN and a starting balance of 0
+            self.user_accounts[account] = {"pin": user_pin, "balance": 0}
+            print(f"Account {account} successfully created.")
