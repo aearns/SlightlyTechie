97606427de983a3bc4fb68d1fcfdeb322ae4a6de
aearns <aearns@gmail.com>
2025-03-07 22:59:13 +0000

    Adding withdraw function

--- Assignments/bank_atm.py
+++ Assignments/bank_atm.py
@@ -71,3 +71,14 @@ class ATM:
         else:
             print("You need to log in first.")
 
+
+    def withdraw(self, amount):
+        if self.logged_in_user:
+            if amount > 0 and self.user_accounts[self.logged_in_user]['balance'] >= amount:
+                self.user_accounts[self.logged_in_user]['balance'] -= amount
+                print(f"Withdrew ${amount}. Your new balance is: ${self.user_accounts[self.logged_in_user]['balance']}")
+            else:
+                print("Insufficient funds or invalid withdrawal amount.")
+        else:
+            print("You need to log in first.")
+
