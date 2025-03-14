06bcbf92c429b2ff576ecdd8eb3dfa5716323e78
aearns <aearns@gmail.com>
2025-03-14 08:30:36 +0000

    add more

--- Assignments/library.sql
+++ Assignments/library.sql
@@ -6,7 +6,6 @@ CREATE TABLE books (
     year_published INT NOT NULL
 );
 
-
 INSERT INTO books (title, author, year_published)
 VALUES
     ('The Beauty and the Beast', 'L. Reed', 1825),
@@ -16,3 +15,29 @@ VALUES
     ('Things Fall Apart', 'Chinua Achebe', 1997);
 
 SELECT * FROM books;
+
+SELECT * FROM books WHERE author = 'Addo D';
+
+UPDATE books SET year_published = 2011 WHERE author = 'Addo D';
+
+Delete from books where title = 'To Kill a Goat'
+
+CREATE TABLE borrowers (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    book_id INT,
+    borrow_date DATE DEFAULT CURRENT_DATE,
+    return_date DATE,
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+INSERT INTO borrowers (name, book_id, return_date) VALUES
+('Nyaniba Ed', 3, '2024-03-20'),
+('John Doe', 2, '2024-03-22'),
+('Bill Agyeman', 1, '2024-04-01');
+
+select * from borrowers;
+
+SELECT books.title, books.author, borrowers.name, borrowers.borrow_date, borrowers.return_date
+FROM books
+LEFT JOIN borrowers ON books.id = borrowers.book_id;
